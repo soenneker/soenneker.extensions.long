36833b5fb5e01e823c9e0e4c586b01ac0f290d22
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:21 +0000

    Tests

--- test/Soenneker.Extensions.Long.Tests/LongExtensionTests.cs
+++ test/Soenneker.Extensions.Long.Tests/LongExtensionTests.cs
@@ -1,7 +1,13 @@
-
+using Soenneker.Tests.Unit;
+using Xunit;
 
 namespace Soenneker.Extensions.Long.Tests;
 
-public class LongExtensionTests
+public class LongExtensionTests : UnitTest
 {
+    [Fact]
+    public void Default()
+    {
+
+    }
 }
