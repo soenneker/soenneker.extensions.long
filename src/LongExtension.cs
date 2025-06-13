4356e81060a0591736785f780edf9d378abf5e4e
Jake Soenneker <jake@soenneker.com>
2025-06-12 20:01:54 -0500

    addt method

--- src/LongExtension.cs
+++ src/LongExtension.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 
 namespace Soenneker.Extensions.Long;
 
@@ -26,8 +28,27 @@ public static class LongExtension
     /// Console.WriteLine(dateTime); // Output: 5/1/2020 12:00:00 AM (Depending on the system's time zone, output might vary)
     /// </code>
     /// </example>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static DateTime ToDateTimeFromUnixTime(this long unixTime)
     {
         return DateTimeOffset.FromUnixTimeSeconds(unixTime).UtcDateTime;
+    }
+
+    /// <summary>
+    /// Converts the specified 64-bit signed integer to a 32-bit signed integer, throwing an <see cref="OverflowException"/>
+    /// if the value is outside the range of the <see cref="Int32"/> type.
+    /// </summary>
+    /// <param name="value">The 64-bit signed integer to convert.</param>
+    /// <returns>The 32-bit signed integer representation of the input value.</returns>
+    /// <exception cref="OverflowException">
+    /// Thrown when <paramref name="value"/> is less than <see cref="Int32.MinValue"/> or greater than <see cref="Int32.MaxValue"/>.
+    /// </exception>
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int ToInt(this long value)
+    {
+        // this cast will throw OverflowException if value is out of Int32 range
+        return checked((int)value);
     }
 }
